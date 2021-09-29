522831c7d63cb1b46ca85a0e3fb3fa9762e4c3d5
Acalltoredemption <acalltoredemption@gmail.com>
2021-09-29 15:57:05 -0700

    initial component setup

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserItem.css';
+
+const UserItem = props => {
+return (
+    <h2>UserItem</h2>
+)
+}
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
 
 const Users = () => {
+    const USERS = [{
+        id: 'u1',
+        name: 'Justin',
+        image: "https://i1.wp.com/oxsci.org/wp-content/uploads/2019/05/SMILE.png?w=1024&ssl=1",
+        places: '2'
+    }];
 return(
-    <h2>Users</h2>
+    <UsersList items={USERS} />
 )
 }
 
