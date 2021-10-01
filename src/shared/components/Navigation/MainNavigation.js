c22ea5f797993a64eb17b913038ef511ea946398
Acalltoredemption <acalltoredemption@gmail.com>
2021-09-30 18:17:52 -0700

    closeDrawer implemented for sidedrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import MainHeader from './MainHeader';
 import SideDrawer from './SideDrawer';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+    const openDrawer = () => {
+        setDrawerIsOpen(true);
+    };
+
+    const closeDrawrer = () => {
+        setDrawerIsOpen(false);
+    }
+
     return (
         <React.Fragment>
-        <SideDrawer>
+            {drawerIsOpen ? <Backdrop  onClick={closeDrawrer} /> : null}
+        {drawerIsOpen ? (<SideDrawer>
             <nav className="main-navigation__drawer-nav">
                 <NavLinks />
             </nav>
-        </SideDrawer>
+        </SideDrawer>) : null}
         <MainHeader>
-            <button className="main-navigation__menu-btn">
+            <button className="main-navigation__menu-btn" onClick={openDrawer}>
                 <span />
                 <span />
                 <span />
