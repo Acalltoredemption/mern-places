c7858275f32aa19ed8eecc4f0d5f0eee44cbed2b
Acalltoredemption <acalltoredemption@gmail.com>
2021-10-04 17:48:31 -0700

    rendering userplaces properly in app with dummy data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   <main>
   <Switch>
   <Route path="/" component={Users} exact/>
+  <Route path="/:userId/places"><UserPlaces /></Route>
   <Route path="/places/new" component={NewPlace} exact/>
   <Redirect to="/" />
   </Switch>
