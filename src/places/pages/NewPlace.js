86b688f4d204e6d47b717abd74c8201896483c83
Acalltoredemption <acalltoredemption@gmail.com>
2021-10-13 16:48:51 -0700

    added description input and importing associated validators

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useCallback} from 'react';
 import './NewPlace.css';
-import {VALIDATOR_REQUIRE} from '../../shared/util/validators';
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators';
 
 import Input from '../../shared/components/FormElements/Input';
 
 const NewPlace = () => {
+    const titleInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+    const descriptionInputHandler = useCallback((id, value, isValid) => {
+
+    }, []);
+
 return (
     <form className="place-form">
         <Input 
@@ -12,7 +19,16 @@ return (
         type="text" 
         label="Title"
          validators={[VALIDATOR_REQUIRE()]} 
-         errorText="Please enter a valid title." />
+         errorText="Please enter a valid title." 
+         onInput={titleInputHandler} />
+
+         <Input 
+        element="textarea" 
+        id="description"
+        label="Description"
+         validators={[VALIDATOR_MINLENGTH(5)]} 
+         errorText="Please enter a valid description (at least 5 characters)." 
+         onInput={descriptionInputHandler} />
     </form>
 )
 }
