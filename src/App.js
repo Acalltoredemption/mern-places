a1998fe6e222dfdf8692bc51357b92ffc000680c
Acalltoredemption <acalltoredemption@gmail.com>
2021-10-18 15:55:25 -0700

    UpdatePlace component implemented

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import UpdatePlace from './places/pages/UpdatePlace';
+
 
 function App() {
   return <BrowserRouter>
@@ -13,6 +15,7 @@ function App() {
   <Route path="/" component={Users} exact/>
   <Route path="/:userId/places"><UserPlaces /></Route>
   <Route path="/places/new" component={NewPlace} exact/>
+  <Route path="/places/:placeId" component={UpdatePlace} exact />
   <Redirect to="/" />
   </Switch>
   </main>
---
+++ src/places/pages/UpdatePlace.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import {useParams} from 'react-router-dom';
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import {VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators';
+
+const DUMMY_PLACES = [
+    
+    {
+    id: "p1",
+    title: 'Empire State Building',
+    description: 'A famous building!',
+    imageUrl: 'https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg',
+    address: 'New York, Ny',
+    location: {
+        lat: 40.7484405,
+        lng: -73.9878584
+    },
+    creator: 'u1'
+},
+{
+    id: "p2",
+    title: 'Empire State Building',
+    description: 'A famous building!',
+    imageUrl: 'https://bsmedia.business-standard.com/_media/bs/img/article/2021-09/20/full/1632080404-7898.jpg',
+    address: 'New York, Ny',
+    location: {
+        lat: 40.7484405,
+        lng: -73.9878584
+    },
+    creator: 'u2'
+}
+];
+
+const UpdatePlace = () => {
+    const placeId = useParams().placeId;
+
+    const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+    if (!identifiedPlace){
+        return (
+            <div className="center">
+            <h2>Could not find place!</h2>
+            </div>
+        );
+    }
+    return (
+    <form>
+        <Input id="title" element="input" type="text" label="Title" validators={[VALIDATOR_REQUIRE()]} errorText="Please enter a valid title." onInput={() => {}} value={identifiedPlace.title} valid={true} />
+        <Input id="description" element="textarea"  label="Description" validators={[VALIDATOR_MINLENGTH(5)]} errorText="Please set a valid description. (min. 5 characters)" onInput={() => {}} value={identifiedPlace.description} valid={true} />
+        <Button type="submit" disabled={true}>UPDATE PLACE</Button>
+    </form>
+    );
+
+};
+
+export default UpdatePlace;
