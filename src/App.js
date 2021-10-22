504ee035a2a6ccf890e6efa3fed4941eb2bbe259
Acalltoredemption <acalltoredemption@gmail.com>
2021-10-21 17:06:22 -0700

    adding authcontext for statewide auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useCallback} from 'react';
 import {BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
 import Users from './user/pages/Users';
 import NewPlace from './places/pages/NewPlace';
@@ -6,10 +6,21 @@ import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
 import Auth from './user/pages/Auth';
+import {AuthContext} from './shared/context/Auth-Context';
 
 
 function App() {
-  return <BrowserRouter>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+  return (
+  <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+  <BrowserRouter>
   <MainNavigation />
   <main>
   <Switch>
@@ -21,7 +32,9 @@ function App() {
   <Redirect to="/" />
   </Switch>
   </main>
-  </BrowserRouter>;
+  </BrowserRouter>
+  </AuthContext.Provider>
+  );
 }
 
 export default App;
